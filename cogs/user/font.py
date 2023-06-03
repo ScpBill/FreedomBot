8818c7d60a181c347a3f419f0157492bcb8d8cb5
ScpBill <artpr.zxc@gmail.com>
2023-06-04 01:52:56 +0500

    add: all user cogs to this project from rinderpest

---
+++ cogs/user/font.py
@@ -0,0 +1,79 @@
+from discord.ext.commands import Cog, Bot, Context, MissingRequiredArgument
+from discord.ext import commands
+from discord import app_commands
+
+
+FONTS = {
+    'regular':                '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    'bold':                   '𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳',
+    'italic':                 '----------𝐴𝐵𝐶𝐷𝐸𝐹𝐺𝐻𝐼𝐽𝐾𝐿𝑀𝑁𝑂𝑃𝑄𝑅𝑆𝑇𝑈𝑉𝑊𝑋𝑌𝑍𝑎𝑏𝑐𝑑𝑒𝑓𝑔ℎ𝑖𝑗𝑘𝑙𝑚𝑛𝑜𝑝𝑞𝑟𝑠𝑡𝑢𝑣𝑤𝑥𝑦𝑧',
+    'bold-italic':            '----------𝑨𝑩𝑪𝑫𝑬𝑭𝑮𝑯𝑰𝑱𝑲𝑳𝑴𝑵𝑶𝑷𝑸𝑹𝑺𝑻𝑼𝑽𝑾𝑿𝒀𝒁𝒂𝒃𝒄𝒅𝒆𝒇𝒈𝒉𝒊𝒋𝒌𝒍𝒎𝒏𝒐𝒑𝒒𝒓𝒔𝒕𝒖𝒗𝒘𝒙𝒚𝒛',
+    'script':                 '----------𝒜ℬ𝒞𝒟ℰℱ𝒢ℋℐ𝒥𝒦ℒℳ𝒩𝒪𝒫𝒬ℛ𝒮𝒯𝒰𝒱𝒲𝒳𝒴𝒵𝒶𝒷𝒸𝒹ℯ𝒻ℊ𝒽𝒾𝒿𝓀𝓁𝓂𝓃ℴ𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏',
+    'bold-script':            '----------𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃',
+    'fraktur':                '----------𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ𝔞𝔟𝔠𝔡𝔢𝔣𝔤𝔥𝔦𝔧𝔨𝔩𝔪𝔫𝔬𝔭𝔮𝔯𝔰𝔱𝔲𝔳𝔴𝔵𝔶𝔷',
+    'double-struck':          '𝟘𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫',
+    'bold-fraktur':           '----------𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟',
+    'sans-serif':             '𝟢𝟣𝟤𝟥𝟦𝟧𝟨𝟩𝟪𝟫𝖠𝖡𝖢𝖣𝖤𝖥𝖦𝖧𝖨𝖩𝖪𝖫𝖬𝖭𝖮𝖯𝖰𝖱𝖲𝖳𝖴𝖵𝖶𝖷𝖸𝖹𝖺𝖻𝖼𝖽𝖾𝖿𝗀𝗁𝗂𝗃𝗄𝗅𝗆𝗇𝗈𝗉𝗊𝗋𝗌𝗍𝗎𝗏𝗐𝗑𝗒𝗓',
+    'sans-serif-bold':        '𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇',
+    'sans-serif-italic':      '----------𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻',
+    'sans-serif-bold-italic': '----------𝘼𝘽𝘾𝘿𝙀𝙁𝙂𝙃𝙄𝙅𝙆𝙇𝙈𝙉𝙊𝙋𝙌𝙍𝙎𝙏𝙐𝙑𝙒𝙓𝙔𝙕𝙖𝙗𝙘𝙙𝙚𝙛𝙜𝙝𝙞𝙟𝙠𝙡𝙢𝙣𝙤𝙥𝙦𝙧𝙨𝙩𝙪𝙫𝙬𝙭𝙮𝙯',
+    'monospace':              '𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣'
+}
+STYLES = [
+    app_commands.Choice(name='𝐁𝐨𝐥𝐝', value='bold'),
+    app_commands.Choice(name='𝐼𝑡𝑎𝑙𝑖𝑐', value='italic'),
+    app_commands.Choice(name='𝑩𝒐𝒍𝒅 𝑰𝒕𝒂𝒍𝒊𝒄', value='bold italic'),
+    app_commands.Choice(name='𝒮𝒸𝓇𝒾𝓅𝓉', value='script'),
+    app_commands.Choice(name='𝓑𝓸𝓵𝓭 𝓢𝓬𝓻𝓲𝓹𝓽', value='bold script'),
+    app_commands.Choice(name='𝔉𝔯𝔞𝔨𝔱𝔲𝔯', value='fraktur'),
+    app_commands.Choice(name='𝔻𝕠𝕦𝕓𝕝𝕖-𝕤𝕥𝕣𝕦𝕔𝕜', value='double-struck'),
+    app_commands.Choice(name='𝕭𝖔𝖑𝖉 𝕱𝖗𝖆𝖐𝖙𝖚𝖗', value='bold fraktur'),
+    app_commands.Choice(name='𝖲𝖺𝗇𝗌-𝗌𝖾𝗋𝗂𝖿', value='sans-serif'),
+    app_commands.Choice(name='𝗦𝗮𝗻𝘀-𝗦𝗲𝗿𝗶𝗳 𝗕𝗼𝗹𝗱', value='sans-serif bold'),
+    app_commands.Choice(name='𝘚𝘢𝘯𝘴-𝘚𝘦𝘳𝘪𝘧 𝘐𝘵𝘢𝘭𝘪𝘤', value='sans-serif italic'),
+    app_commands.Choice(name='𝙎𝙖𝙣𝙨-𝙎𝙚𝙧𝙞𝙛 𝘽𝙤𝙡𝙙 𝙄𝙩𝙖𝙡𝙞𝙘',
+                        value='sans-serif bold italic'),
+    app_commands.Choice(name='𝙼𝚘𝚗𝚘𝚜𝚙𝚊𝚌𝚎', value='monospace')
+]
+
+
+async def font_style_autocomplete(ctx, current: str):
+    return [style for style in STYLES if current.lower() in style.value]
+
+
+# todo: FontCog
+class FontCog(Cog, name='Features'):
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    @app_commands.describe(style='Style font of your text, long name is separated with `-`', text='Your text')
+    @app_commands.autocomplete(style=font_style_autocomplete)
+    async def font(self, ctx: Context, style: str, *, text: str):
+        """The font generator: write and copy!"""
+
+        # Wait message
+        await ctx.defer()
+        if (style := style.lower().replace(' ', '-')) not in FONTS.keys():
+            return await ctx.reply(
+                '**The font is specified incorrectly.**\n*Available fonts*: %s'
+                % ' '.join([f'`{name}`' for name in FONTS.keys()]))
+
+        # Replace symbols
+        alphabet, decorated = FONTS['regular'], FONTS[style]
+        try:
+            await ctx.reply(''.join([decorated[alphabet.index(char)] if
+                                     char in alphabet and decorated[alphabet.index(char)] != '-' else char
+                                     for char in text]))
+        except IndexError:
+            await ctx.reply('Sorry, an unintentional error has occurred.')
+
+    @font.error
+    async def argument_error(self, ctx: Context, error):
+        if isinstance(error, MissingRequiredArgument):
+            await ctx.send_help(ctx.command)
+
+
+async def setup(bot: Bot) -> None:
+    await bot.add_cog(FontCog(bot))
