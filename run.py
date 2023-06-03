d72b10bd671906660b2494871755f16e1a4b8957
Rinny <122083220+ScpBill@users.noreply.github.com>
2023-06-03 19:48:35 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+import os
+
+
 def main():
     from bot import start_bot
     start_bot()
 
 
 if __name__ == '__main__':
+    os.system('git reset --hard')
+    os.system('git pull origin master')
     main()
