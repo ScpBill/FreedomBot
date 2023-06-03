252f6f71a365d167cdaa13cf5f3df70ff2291167
ScpBill <artpr.zxc@gmail.com>
2023-06-04 01:33:54 +0500

    add: list_guilds command

---
+++ cogs/owner/analytics.py
@@ -0,0 +1,32 @@
+from discord.ext.commands import Cog, Bot, Context
+from discord.app_commands import Choice
+from discord.ext import commands
+from discord import app_commands, Member, Embed, Colour
+
+from spec.config import Config
+
+import os
+
+
+class Analytics(Cog):
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    async def list_guilds(self, ctx: Context) -> None:
+        guilds = []
+        for guild in self.bot.guilds:
+            invite = await guild.channels[0].create_invite(max_uses=1, temporary=True, max_age=600)
+            guilds.append(f'- [{guild.name}]({invite})')
+        embed = Embed(
+            title='List of guilds with me',
+            description='All invitation links are valid for 10 minutes and once.\n' +
+            '\n'.join(guild)
+        )
+        
+        await ctx.reply(embed, ephemeral=True)
+
+
+async def setup(bot: Bot) -> None:
+    await bot.add_cog(Analytics(bot))
