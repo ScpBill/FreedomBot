f18c0c6cd5901a0d2cb91f908f0a3f8860016da7
ScpBill <artpr.zxc@gmail.com>
2023-06-05 18:32:03 +0500

    remove convert from ClearConverter

--- cogs/admin/clear.py
+++ cogs/admin/clear.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Cog, Bot, Context, FlagConverter, BadArgument, Converter
+from discord.ext.commands import Cog, Bot, Context, FlagConverter, BadArgument, Converter, MissingRequiredArgument
 from discord import Member, Message
 from discord.ext import commands
 from discord import app_commands
@@ -36,28 +36,6 @@ class ClearConverter(FlagConverter):
     time: TimePeriodConverter = commands.flag(name='time', default=None)
     members: commands.Greedy[Member] = commands.flag(name='member', default=[])
     
-    async def convert(self, ctx: Context, *, argument: str):
-        timedelta = datetime.timedelta()
-        # [1, w, 2, d, 3, h, 4, m, 5, s]
-        parts: list[str] = re.findall(r'\D+|\d+', argument.replace(' ', ''))
-        for num, form in zip(parts[::2], parts[1::2]):
-            if not num.isdigit() or form.isdigit():
-                raise BadArgument()
-            if form in ('w', 'weak', 'weaks', 'н', 'неделя', 'недели', 'недель'):
-                timedelta += datetime.timedelta(days=7*num)
-            elif form in ('d', 'day', 'days', 'д', 'день', 'дня', 'дней', 'сутки', 'суток'):
-                timedelta += datetime.timedelta(days=num)
-            elif form in ('h', 'hour', 'hours', 'ч', 'час', 'часа', 'часов'):
-                timedelta += datetime.timedelta(hours=num)
-            elif form in ('m', 'minute', 'minutes', 'м', 'минута', 'минуты', 'минут'):
-                timedelta += datetime.timedelta(minutes=num)
-            elif form in ('s', 'second', 'seconds', 'с', 'секунда', 'секунды', 'секунд'):
-                timedelta += datetime.timedelta(seconds=num)
-            else:
-                raise BadArgument()
-        self.time = ctx.message.created_at - timedelta
-        return self
-    
 class Clear(Cog):
 
     def __init__(self, bot: Bot):
@@ -86,6 +64,10 @@ class Clear(Cog):
         )
 
         await answer.edit(content='✅ Cleaning was successful!', delete_after=5.0)
+    
+    @clear.error
+    async def clear_error(ctx: Context, error):
+        if isinstance(error, AssertionError) or isinstance(error, MissingRequiredArgument) or isinstance(error, )
 
 
 async def setup(bot: Bot) -> None:
